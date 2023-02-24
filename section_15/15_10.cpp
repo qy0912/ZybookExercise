b04fa78212ed6570ef3b5f96d24265deacf52977
qy0912 <qy434725138@hotmail.com>
2023-02-24 11:19:09 +1100

    added bubble sort

--- section_15/15_10.cpp
+++ section_15/15_10.cpp
@@ -5,12 +5,29 @@ using namespace std;
 // TODO: Write a void function SelectionSortDescendTrace() that takes
 //       an integer array and the number of elements in the array as arguments,
 //       and sorts the array into descending order.
+void BubbleSort(int numbers [], int numElements){
+   for(int i = 0 ; i < numElements ; i++){
+      for(int j = 0 ; j < numElements-1 ; j++){
+         if(numbers[j]>numbers[j+1]){
+            int temp = numbers[j+1];
+            numbers[j+1] = numbers[j];
+            numbers[j] = temp;
+         }
+      }
+
+      for(int k = 0 ; k < numElements ; k++){
+         cout<<numbers[k]<<" ";
+      }
+      cout<<endl;
+   }
+}
+
 void SelectionSortDescendTrace(int numbers [], int numElements) {
    for(int i = 0 ; i < numElements ; i++){
       int max = numbers[i];
       int index = i;
       for(int j = i+1; j<numElements ; j++){
-         if(max<numbers[j]){
+         if(max>numbers[j]){
             max = numbers[j];
             index = j;
          }
@@ -41,7 +58,7 @@ int main() {
       cin>>input;
    }
    
-   SelectionSortDescendTrace(numbers,i);
+   BubbleSort(numbers,i);
    // TODO: Read in a list of up to 10 positive integers; stop when
    //       -1 is read. Then call SelectionSortDescendTrace() function.
    
